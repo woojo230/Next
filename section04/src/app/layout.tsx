25153032997f90a14b99b5746b883d5c9fbe8b24
woojo230 <woojunghyck@naver.com>
2025-03-06 22:41:00 +0900

    request memoziation

--- section04/src/app/layout.tsx
+++ section04/src/app/layout.tsx
@@ -1,6 +1,25 @@
-import "./globals.css";
-import Link from "next/link";
-import style from "./layout.module.css";
+import './globals.css';
+import Link from 'next/link';
+import style from './layout.module.css';
+import { BookData } from '@/types';
+
+async function Footer() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+  );
+  if (!response.ok) {
+    return <div>제작 @woojo230</div>;
+  }
+
+  const bookData: BookData[] = await response.json();
+  const bookCount = bookData.length;
+  return (
+    <footer>
+      <div>제작 @woojo230</div>
+      <div>{bookCount}개의 도서가 등록되어 있습니다</div>
+    </footer>
+  );
+}
 
 export default function RootLayout({
   children,
@@ -12,10 +31,10 @@ export default function RootLayout({
       <body>
         <div className={style.container}>
           <header>
-            <Link href={"/"}>📚 ONEBITE BOOKS</Link>
+            <Link href={'/'}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @winterlood</footer>
+          <Footer />
         </div>
       </body>
     </html>
